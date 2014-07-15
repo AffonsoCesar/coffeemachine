14b1247468310ac0d5612ceb2aeb9a65c1b148e9
Affonso César <affonso.souza@dce.ufpb.br>
2014-07-15 08:52:05 -0300

    Task 1.01 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,5 +1,12 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
-public class MyCoffeeMachine{
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
+public class MyCoffeeMachine implements CoffeeMachine {
+
+	public MyCoffeeMachine(ComponentsFactory factory) {
+		factory.getDisplay().info("Insert coins and select a drink!");
+	}
+	
 }
