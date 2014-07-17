9438397c0cb9f49754b15dddba3d4817383bae72
Affonso César <affonso.souza@dce.ufpb.br>
2014-07-17 10:25:51 -0300

    Task 1.02 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,34 +1,31 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
-import static org.mockito.Mockito.verify;
-
-import org.junit.Before;
-
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
-import br.ufpb.dce.aps.coffeemachine.Display;
 import br.ufpb.dce.aps.coffeemachine.MockComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
 	private ComponentsFactory factory;
-	private Display display;
 
-	@Before
+
 	public void init() {
 		factory = new MockComponentsFactory();
-		display = factory.getDisplay();
+		
 	}
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
-		factory.getDisplay().info("Insert coins and select a drink!");
+		this.factory = factory;
+		this.factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
-	public void insertCoin(Coin dime) {
-		factory.getDisplay().info("Total: US$ 0.10");
+	public void insertCoin(Coin coin) {
+		int valorTotal = coin.getValue();
+		int dolar = valorTotal /100;
+	    int decCent = valorTotal % 100;
+		this.factory.getDisplay().info ("Total: US$ "+dolar+"." + decCent);
 	}
-	
 }
 
 	
